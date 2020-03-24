636d088cbab0570eb7f0f85cf46b6e54a9b36fcd
aborgra <audrey.borgra@gmail.com>
2020-03-24 09:14:19 -0500

    tested and runs

--- DepartmentsEmployees/Program.cs
+++ DepartmentsEmployees/Program.cs
@@ -10,138 +10,142 @@ namespace DepartmentsEmployees
         static void Main(string[] args)
         {
 
-
-            Console.WriteLine("What would you like to do?");
-            Console.WriteLine("");
-            Console.WriteLine("1. Show all departments");
-            Console.WriteLine("2. Show specific department by Id");
-            Console.WriteLine("3. Add new department");
-            Console.WriteLine("4. Update department information");
-            Console.WriteLine("5. Delete department by Id");
-            Console.WriteLine("6. Show all employees");
-            Console.WriteLine("7. Show specific employee by Id");
-            Console.WriteLine("8. Show all employees by department");
-            Console.WriteLine("9. Add new employee");
-            Console.WriteLine("10. Update employee information");
-            Console.WriteLine("11. Delete employee by Id");
-
-            var choice = Console.ReadLine();
-            DepartmentRepository departmentRepo = new DepartmentRepository();
-            EmployeeRepository employeeRepo = new EmployeeRepository();
-
-            switch (Int32.Parse(choice))
+            while (true)
             {
-                case 1:
 
-                    Console.WriteLine("Getting All Departments:");
-                    Console.WriteLine();
 
-                    List<Department> allDepartments = departmentRepo.GetAllDepartments();
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("");
+                Console.WriteLine("1. Show all departments");
+                Console.WriteLine("2. Show specific department by Id");
+                Console.WriteLine("3. Add new department");
+                Console.WriteLine("4. Update department information");
+                Console.WriteLine("5. Delete department by Id");
+                Console.WriteLine("6. Show all employees");
+                Console.WriteLine("7. Show specific employee by Id");
+                Console.WriteLine("8. Show all employees by department");
+                Console.WriteLine("9. Add new employee");
+                Console.WriteLine("10. Update employee information");
+                Console.WriteLine("11. Delete employee by Id");
 
-                    foreach (Department dept in allDepartments)
-                    {
-                        Console.WriteLine($"{dept.Id} {dept.DeptName}");
-                    }
-                    break;
+                var choice = Console.ReadLine();
+                DepartmentRepository departmentRepo = new DepartmentRepository();
+                EmployeeRepository employeeRepo = new EmployeeRepository();
 
-                case 2:
-                    Console.WriteLine("Enter Id of department");
-                    var departmentChoice = int.Parse(Console.ReadLine());
-                    Console.WriteLine($"Getting department with Id of {departmentChoice}");
-                    Department singleDepartment = departmentRepo.GetDepartmentById(departmentChoice);
+                switch (Int32.Parse(choice))
+                {
+                    case 1:
 
-                    Console.WriteLine($"{singleDepartment.Id} {singleDepartment.DeptName}");
-                    break;
+                        Console.WriteLine("Getting All Departments:");
+                        Console.WriteLine();
 
-                case 3:
-                    Console.WriteLine("Enter the name of the new department");
-                    var departmentName = Console.ReadLine();
-                    Department newDepartment = new Department
-                    {
-                        DeptName = departmentName
-                    };
+                        List<Department> allDepartments = departmentRepo.GetAllDepartments();
 
-                    departmentRepo.AddDepartment(newDepartment);
-                    Console.WriteLine($"Added the new {departmentName} Department!");
-                    break;
+                        foreach (Department dept in allDepartments)
+                        {
+                            Console.WriteLine($"{dept.Id} {dept.DeptName}");
+                        }
+                        break;
 
-                case 4:
-                    Console.WriteLine("Update Department Info");
-                    //TBD
-                    break;
+                    case 2:
+                        Console.WriteLine("Enter Id of department");
+                        var departmentChoice = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Getting department with Id of {departmentChoice}");
+                        Department singleDepartment = departmentRepo.GetDepartmentById(departmentChoice);
 
-                case 5:
-                    Console.WriteLine("Enter the Id of the department you would like to delete");
-                    var deleteDeptChoice =int.Parse(Console.ReadLine());
+                        Console.WriteLine($"{singleDepartment.Id} {singleDepartment.DeptName}");
+                        break;
 
-                    departmentRepo.DeleteDepartment(deleteDeptChoice);
-                    Console.WriteLine($"Department with the Id of{deleteDeptChoice} has been deleted.");
-                    break;
+                    case 3:
+                        Console.WriteLine("Enter the name of the new department");
+                        var departmentName = Console.ReadLine();
+                        Department newDepartment = new Department
+                        {
+                            DeptName = departmentName
+                        };
 
-                case 6:
-                    Console.WriteLine("Getting All Employees:");
-                    Console.WriteLine();
+                        departmentRepo.AddDepartment(newDepartment);
+                        Console.WriteLine($"Added the new {departmentName} Department!");
+                        break;
 
-                    List<Employee> allEmployees = employeeRepo.GetAllEmployees();
+                    case 4:
+                        Console.WriteLine("Update Department Info");
+                        //TBD
+                        break;
 
-                    foreach (Employee emp in allEmployees)
-                    {
-                        Console.WriteLine($"{emp.FirstName} {emp.LastName}");
-                    }
+                    case 5:
+                        Console.WriteLine("Enter the Id of the department you would like to delete");
+                        var deleteDeptChoice = int.Parse(Console.ReadLine());
 
-                    break;
-                case 7:
-                    Console.WriteLine("Enter the Id of the employee you would like to view");
-                    var chosenEmployeeId = int.Parse(Console.ReadLine());
-                    Employee singleEmployee = employeeRepo.GetEmployeeById(chosenEmployeeId);
+                        departmentRepo.DeleteDepartment(deleteDeptChoice);
+                        Console.WriteLine($"Department with the Id of{deleteDeptChoice} has been deleted.");
+                        break;
 
-                    Console.WriteLine($"Getting Employee with Id of {chosenEmployeeId}.");
+                    case 6:
+                        Console.WriteLine("Getting All Employees:");
+                        Console.WriteLine();
 
-                    Console.WriteLine($"{singleEmployee.Id} {singleEmployee.FirstName} {singleEmployee.LastName}");
-                    break;
+                        List<Employee> allEmployees = employeeRepo.GetAllEmployees();
 
-                case 8:
-                    List<Employee> allDepartmentEmployees = employeeRepo.GetAllEmployeesWithDepartment();
+                        foreach (Employee emp in allEmployees)
+                        {
+                            Console.WriteLine($"{emp.FirstName} {emp.LastName}");
+                        }
 
-                    foreach (Employee emp in allDepartmentEmployees)
-                    {
-                        Console.WriteLine($"{emp.FirstName} {emp.LastName} - {emp.Department.DeptName}");
-                    }
-                    break;
+                        break;
+                    case 7:
+                        Console.WriteLine("Enter the Id of the employee you would like to view");
+                        var chosenEmployeeId = int.Parse(Console.ReadLine());
+                        Employee singleEmployee = employeeRepo.GetEmployeeById(chosenEmployeeId);
 
-                case 9:
-                    Console.WriteLine("Enter new employee's first name");
-                    var newEmployeeFirstName = Console.ReadLine();
-                    Console.WriteLine("Enter new employee's last name");
-                    var newEmployeeLastName = Console.ReadLine();
-                    Console.WriteLine("Enter new employee's department Id");
-                    var newEmployeeDeptId = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Getting Employee with Id of {chosenEmployeeId}.");
 
-                    Employee newEmployee = new Employee
-                    {
-                        FirstName = newEmployeeFirstName,
-                        LastName = newEmployeeLastName,
-                        DepartmentId = newEmployeeDeptId
-                    };
+                        Console.WriteLine($"{singleEmployee.Id} {singleEmployee.FirstName} {singleEmployee.LastName}");
+                        break;
 
-                    employeeRepo.AddEmployee(newEmployee);
-                    Console.WriteLine($"Added {newEmployee.FirstName} {newEmployee.LastName} as a new employee!");
-                    break;
+                    case 8:
+                        List<Employee> allDepartmentEmployees = employeeRepo.GetAllEmployeesWithDepartment();
 
-                case 10:
-                    Console.WriteLine("Update Employee Info");
-                    //TBD
-                    break;
+                        foreach (Employee emp in allDepartmentEmployees)
+                        {
+                            Console.WriteLine($"{emp.FirstName} {emp.LastName} - {emp.Department.DeptName}");
+                        }
+                        break;
 
-                case 11:
-                    Console.WriteLine("Enter the Id of the employee you would like to delete");
-                    var deleteEmployeeId = int.Parse(Console.ReadLine());
-                    employeeRepo.DeleteEmployee(deleteEmployeeId);
-                    Console.WriteLine($"Employee with the Id of {deleteEmployeeId} has been deleted");
-                    break;
+                    case 9:
+                        Console.WriteLine("Enter new employee's first name");
+                        var newEmployeeFirstName = Console.ReadLine();
+                        Console.WriteLine("Enter new employee's last name");
+                        var newEmployeeLastName = Console.ReadLine();
+                        Console.WriteLine("Enter new employee's department Id");
+                        var newEmployeeDeptId = int.Parse(Console.ReadLine());
 
-                default:
-                    break;
+                        Employee newEmployee = new Employee
+                        {
+                            FirstName = newEmployeeFirstName,
+                            LastName = newEmployeeLastName,
+                            DepartmentId = newEmployeeDeptId
+                        };
+
+                        employeeRepo.AddEmployee(newEmployee);
+                        Console.WriteLine($"Added {newEmployee.FirstName} {newEmployee.LastName} as a new employee!");
+                        break;
+
+                    case 10:
+                        Console.WriteLine("Update Employee Info");
+                        //TBD
+                        break;
+
+                    case 11:
+                        Console.WriteLine("Enter the Id of the employee you would like to delete");
+                        var deleteEmployeeId = int.Parse(Console.ReadLine());
+                        employeeRepo.DeleteEmployee(deleteEmployeeId);
+                        Console.WriteLine($"Employee with the Id of {deleteEmployeeId} has been deleted");
+                        break;
+
+                    default:
+                        break;
+                }
             }
 
 
